c92704193287a39bbc211d4cc6cf06b942fe309d
rbpolim <rbpolim@gmail.com>
2023-09-29 15:21:13 -0300

    feat: transformed rbpolim into a single component logo

---
+++ components/logo.tsx
@@ -0,0 +1,10 @@
+export const Logo = () => {
+  return (
+    <div className="items-center hidden text-4xl font-black md:flex">
+      <h2>
+        rbpolim
+        <span className="animate-ping">_</span>
+      </h2>
+    </div>
+  )
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,12 @@
+import { Logo } from "@/components/logo"
 import { ModeToggle } from "@/components/mode-toggle"
 import { NavRoutes } from "@/components/nav-routes"
 import { MobileSidebar } from "@/components/mobile-sidebar"
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl p-4 mx-auto">
-      <h2 className="hidden text-4xl font-black md:flex">
-        rbpolim_
-      </h2>
+    <div className="flex items-center justify-between w-full max-w-6xl p-4 mx-auto">
+      <Logo />
       <MobileSidebar />
       <NavRoutes />
       <ModeToggle />
