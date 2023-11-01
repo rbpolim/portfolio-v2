a4910c37c98e992860e0ffa940811ae6f166159d
rbpolim <rbpolim@gmail.com>
2023-11-01 09:33:29 -0300

    feat: new section skills w/ basic style

---
+++ components/sections/skills-section.tsx
@@ -0,0 +1,15 @@
+export const SkillsSection = () => {
+  return (
+    <section className="min-h-[calc(100vh-100px)]">
+      <div className="flex items-center gap-x-4">
+        <h2 className="font-black text-7xl">
+          skills
+        </h2>
+        <div className="w-full h-1 rounded-full bg-foreground" />
+      </div>
+      <div className="grid grid-cols-1 gap-10 mt-16 md:grid-cols-2">
+        <p>skills</p>
+      </div>
+    </section>
+  )
+}
