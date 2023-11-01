423a43b29abd63b01ee66f131e9a4eb451c01767
rbpolim <rbpolim@gmail.com>
2023-11-01 09:34:44 -0300

    feat: new prop size logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,22 @@
-export const Logo = () => {
+import { cn } from '@/lib/utils'
+
+type LogoProps = {
+  size?: 'sm' | 'md' | 'lg'
+}
+
+export const Logo = ({
+  size = 'md',
+}: LogoProps) => {
   return (
-    <div className="items-center hidden text-4xl font-black md:flex">
-      <h2>
+    <div className="items-center hidden md:flex">
+      <h2 className={cn(
+        'font-black',
+        size === 'sm' && 'text-2xl',
+        size === 'md' && 'text-4xl',
+        size === 'lg' && 'text-6xl',
+      )}>
         rbpolim
-        <span className="animate-ping">_</span>
+        <span className="duration-1000 animate-ping">_</span>
       </h2>
     </div>
   )
