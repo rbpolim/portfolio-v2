1c450cb7ddf35edda93885a50887bb3b180de37f
rbpolim <rbpolim@gmail.com>
2023-11-06 17:50:17 -0300

    feat: added earth-canvas into hero-section

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,30 +1,35 @@
 import { Button } from "@/components/ui/button"
+import { EarthCanvas } from '@/components/earth-canvas'
 
 export const HeroSection = () => {
+
   return (
     <section className="h-[calc(100vh-100px)]">
-      <div className="flex flex-col h-full justify-evenly">
-        <div className="space-y-4">
-          <div className="flex flex-col items-start text-2xl md:flex-row gap-x-2">
-            <p className="font-bold">
-              Hi, I am Rodrigo Polim.
-            </p>
-            <p className="font-light">
-              Welcome to my website
+      <div className="grid h-full grid-cols-2 pt-32 place-items-center">
+        <div className="flex flex-col items-start justify-between h-full max-h-96">
+          <div className="space-y-4">
+            <div className="flex flex-col items-start text-2xl">
+              <p className="font-bold">
+                Hi, I am Rodrigo Polim.
+              </p>
+              <p className="font-light">
+                Welcome to my website
+              </p>
+            </div>
+            <h1 className="text-6xl font-black">
+              Fullstack Developer
+            </h1>
+            <p className="text-lg font-bold md:text-xl">
+              Passionate about technology and it&apos;s possibilities
             </p>
           </div>
-          <h1 className="text-6xl font-black">
-            Fullstack Developer
-          </h1>
-          <p className="text-lg font-bold md:text-xl">
-            Passionate about technology and it&apos;s possibilities
-          </p>
+          <div>
+            <Button size='lg'>
+              GET IN TOUCH
+            </Button>
+          </div>
         </div>
-        <div>
-          <Button size='lg'>
-            GET IN TOUCH
-          </Button>
-        </div>
+        <EarthCanvas />
       </div>
     </section>
   )
