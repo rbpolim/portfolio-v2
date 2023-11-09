a61b6267a24c580ea6ffa6ea30f99accca2676b0
rbpolim <rbpolim@gmail.com>
2023-11-09 17:54:40 -0300

    chore: created constant folder (projects data)

---
+++ constants/projects.ts
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    title: "Aeras",
+    description: "MVC application to manage recipes, users and chefs. Built with Javascript and PostgreSQL.",
+    techs: ["Javascript", "PostgreSQL", "HTML", "CSS"],
+    buttons: [
+      { label: "VIEW CODE", href: "/" }
+    ]
+  },
+  {
+    title: "Foodfy",
+    description: "MVC application to manage recipes, users and chefs. Built with Javascript and PostgreSQL.",
+    techs: ["Javascript", "PostgreSQL", "HTML", "CSS"],
+    buttons: [
+      { label: "VIEW CODE", href: "/" }
+    ]
+  },
+  {
+    title: "Foodfy",
+    description: "MVC application to manage recipes, users and chefs. Built with Javascript and PostgreSQL.",
+    techs: ["Javascript", "PostgreSQL", "HTML", "CSS"],
+    buttons: [
+      { label: "VIEW CODE", href: "/" },
+      { label: "VIEW CODE", href: "/" }
+    ]
+  },
+]
+
+export { projects }
