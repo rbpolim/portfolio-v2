f37d0eb267e1e87462d940e3f6862ad2e18dcdd9
rbpolim <rbpolim@gmail.com>
2023-11-13 23:49:54 -0300

    fix: adjusted to deploy

--- components/sections/skills-section.tsx
+++ components/sections/skills-section.tsx
@@ -1,10 +1,10 @@
-import { Heading } from "@/components/heading"
+// import { Heading } from "@/components/heading"
 import { Bounded } from "@/components/bounded"
 
 export const SkillsSection = () => {
   return (
     <Bounded>
-      <Heading title="skills" />
+      {/* <Heading title="skills" /> */}
       <div className="grid grid-cols-1 gap-10 mt-16 md:grid-cols-2">
         <p>skills</p>
       </div>
