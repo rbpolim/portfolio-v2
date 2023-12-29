e4b6f2200bcd9a5d0b582d9d735b12115086a28c
rbpolim <rbpolim@gmail.com>
2023-12-29 19:48:21 -0300

    chore: some improves

--- app/(site)/(routes)/about/page.tsx
+++ app/(site)/(routes)/about/page.tsx
@@ -1,32 +1,158 @@
-import { Text } from "@/components/text"
-import { Heading } from "@/components/heading"
-import { Bounded } from "@/components/bounded"
-import { ContactForm } from '@/components/contact-form'
+import Image from "next/image";
+
+import { Text } from "@/components/text";
+import { Heading } from "@/components/heading";
+import { Bounded } from "@/components/bounded";
+import { ContactForm } from "@/components/contact-form";
+import { Separator } from "@/components/ui/separator";
 
 const AboutPage = () => {
   return (
     <Bounded className="py-28">
-      <div className="w-full max-w-2xl mx-auto space-y-8">
-        <Heading>About</Heading>
-        <Heading size="sm" asChild>
-          <h3>Background</h3>
-        </Heading>
-        <Text>
-          I&apos;m a software engineer with a background in design. I&apos;ve worked on projects ranging from small business websites to enterprise applications for Fortune 500 companies.
-        </Text>
-        <Text>
-          I&apos;m a software engineer with a background in design. I&apos;ve worked on projects ranging from small business websites to enterprise applications for Fortune 500 companies.
-        </Text>
-        <Heading size="sm" asChild>
-          <h3>Interest</h3>
-        </Heading>
-        <Text>
-          I&apos;m interested in the intersection of design and technology. I believe that the best products are built by teams that value both.
-        </Text>
+      <div className="w-full max-w-2xl mx-auto flex flex-col my-20 gap-y-20">
+        <div className="space-y-8">
+          <Heading className="font-black">About</Heading>
+          <Heading size="sm" asChild>
+            <h3>Background</h3>
+          </Heading>
+          <Text>
+            I&apos;m a software engineer with a background in design. I&apos;ve
+            worked on projects ranging from small business websites to
+            enterprise applications for Fortune 500 companies.
+          </Text>
+          <Text>
+            I&apos;m a software engineer with a background in design. I&apos;ve
+            worked on projects ranging from small business websites to
+            enterprise applications for Fortune 500 companies.
+          </Text>
+          <Heading size="sm" asChild>
+            <h3>Interest</h3>
+          </Heading>
+          <Text>
+            I&apos;m interested in the intersection of design and technology. I
+            believe that the best products are built by teams that value both.
+          </Text>
+        </div>
+        <Separator />
+        <div className="space-y-8">
+          <Heading className="font-black">Tools & Technologies</Heading>
+          <Text>Here are some of the tools I use most frequently.</Text>
+          <div className="grid grid-cols-4 gap-10 pt-10">
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+            <div className="flex flex-col items-center gap-y-1">
+              <Image
+                src={"/icons/typescript.svg"}
+                width={60}
+                height={60}
+                alt={"typescript"}
+              />
+              <Text>TypeScript</Text>
+            </div>
+          </div>
+        </div>
+        <Separator />
         <ContactForm />
       </div>
     </Bounded>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,15 +1,15 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
-import { useForm } from "react-hook-form"
-import { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { z } from "zod"
-import { toast } from "sonner"
-import axios from "axios"
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { toast } from "sonner";
+import axios from "axios";
 
-import { Input } from "@/components/ui/input"
-import { Button } from '@/components/ui/button'
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -17,58 +17,55 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage
-} from "@/components/ui/form"
-import { Heading } from "@/components/heading"
-import { Textarea } from "@/components/ui/textarea"
+  FormMessage,
+} from "@/components/ui/form";
+import { Heading } from "@/components/heading";
+import { Textarea } from "@/components/ui/textarea";
 
 const schema = z.object({
   name: z.string().trim().min(2).max(20),
   email: z.string().trim().email(),
-  message: z.string().trim().min(10).max(1000)
-})
+  message: z.string().trim().min(10).max(1000),
+});
 
-type FormValues = z.infer<typeof schema>
+type FormValues = z.infer<typeof schema>;
 
 export const ContactForm = () => {
-  const router = useRouter()
-  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm({
     resolver: zodResolver(schema),
     defaultValues: {
       name: "",
       email: "",
-      message: ""
-    }
-  })
+      message: "",
+    },
+  });
 
   const onSubmit = async (values: FormValues) => {
     try {
-      setIsLoading(true)
+      setIsLoading(true);
 
-      await axios.post("/api/send", values)
+      await axios.post("/api/send", values);
 
-      router.refresh()
-      router.push('/');
-      toast.success("The message has been sent")
+      router.refresh();
+      router.push("/");
+      toast.success("The message has been sent");
     } catch (error) {
-      console.error(error)
-      toast.error("Something went wrong!")
+      console.error(error);
+      toast.error("Something went wrong!");
     } finally {
-      form.reset()
-      setIsLoading(false)
+      form.reset();
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
-    <>
-      <Heading>Get in touch</Heading>
+    <div className="space-y-8">
+      <Heading className="font-black">Get in touch</Heading>
       <Form {...form}>
-        <form
-          className="space-y-6"
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
+        <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
           <div className="grid grid-cols-1 gap-4">
             <FormField
               control={form.control}
@@ -118,8 +115,8 @@ export const ContactForm = () => {
                     />
                   </FormControl>
                   <FormDescription>
-                    Don&apos;t worry!
-                    Your email won&apos;t be used for marketing or spam by me.
+                    Don&apos;t worry! Your email won&apos;t be used for
+                    marketing or spam by me.
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
@@ -127,6 +124,7 @@ export const ContactForm = () => {
             />
           </div>
           <Button
+            size="lg"
             type="submit"
             disabled={isLoading}
             className="w-full"
@@ -135,6 +133,6 @@ export const ContactForm = () => {
           </Button>
         </form>
       </Form>
-    </>
-  )
-}
+    </div>
+  );
+};
