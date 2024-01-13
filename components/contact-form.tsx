d10dd590306dd7df395b10bee926b3261eee48b3
rbpolim <rbpolim@gmail.com>
2024-01-13 19:31:51 -0300

    chore: some changes

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { toast } from "sonner";
